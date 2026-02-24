8388e893ea68c131db6aa9e404cd7b3489358414
botakkilat001-a11y <botakkilat001@gmail.com>
2026-02-24 11:19:52 +0800

    Add files via upload

---
+++ kategori_tambah.php
@@ -0,0 +1,49 @@
+<div class="container-fluid">
+    <h1 class="h3 rb-4 text-gray-800">kategori</h1>
+    <div class="chart-shadow rb-4">
+        <div class="card-body">
+            <form action="" method="POST">
+                <?php
+                if(isset($_POST['submit'])) {
+                    $kategori = strtolower($_POST['kategori']);
+                    $cek = mysqli_query($koneksi, "SELECT * FROM kategori WHERE LOWER(kategori)='$kategori'");
+                    $check = mysqli_num_rows($cek);
+                    if ($check > 0) {
+                        echo "data yang dimasukkan sama";
+
+                    }else {
+                        $query = mysqli_query($koneksi, "INSERT INTO kategori (kategori) VALUES ('$kategori')");
+                        if ($query) {
+                            echo '<script>alert("tambah data berhasil"); </script>';
+                        } else {
+                            echo '<script>alert("tambah data gagal"); </script>';
+                        }
+                    }
+                }
+                   
+                ?>
+                <div class="row mb-3">
+                    <label class="col-md-3 col-from-label">nama kategori</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" name="kategori" placeholder="masukan nama kategori" required>
+
+                    </div>
+
+                </div>
+                <div class="row">
+                    <div class="col-md-9 offsset-md-3">
+                        <button type="submit" class="btn btn-primary" name="submit" value="submit">
+                                simpan
+                        </button>
+                        <button type="reset" class="btn btn-secondary">
+                                reset
+                        </button>
+                        <a href="?page=kategori" class="btn btn-danger">
+                                kembali
+                        </a>
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+</div>
